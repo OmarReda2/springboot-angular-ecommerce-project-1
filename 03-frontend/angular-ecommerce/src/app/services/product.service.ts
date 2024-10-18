0b770b284656b1dc3982a63ba202fbc000191252
Omar Reda <omarredaelsayied@gmail.com>
2024-10-18 13:53:38 +0300

    Dynamic Search Component - angular - updating service and adding html menu component

--- 03-frontend/angular-ecommerce/src/app/services/product.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -1,3 +1,4 @@
+import { ProductCategory } from './../common/product-category';
 import { HttpClient} from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map, Observable } from 'rxjs';
@@ -7,19 +8,38 @@ import { Product } from '../common/product';
   providedIn: 'root'
 })
 export class ProductService {
+  
 
   private baseUrl = 'http://localhost:8080/api/products'
+  private categoryUrl = 'http://localhost:8080/api/product-category'
 
   constructor(private httpClient:HttpClient) { }
 
+
+
+
+
   getProductList(theCategoryId: number): Observable<Product[]> {
+    
+    //need t0 build URL based on category id
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`
 
-    //need t0 build URL based on category id
-
-    return this.httpClient.get<GetResponse>(searchUrl).pipe(
+    return this.httpClient.get<GetProductResponse>(searchUrl).pipe(
       map(response => response._embedded.products)
     )
+  }
+
+
+
+
+
+
+
+  getProductCategories():Observable<ProductCategory[]> {
+    
+    return this.httpClient.get<GetProductCategoryResponse>(this.categoryUrl).pipe(
+      map(response => response._embedded.productCategory)
+    );
   }
 }
 
@@ -27,8 +47,14 @@ export class ProductService {
 // .pipe: used to combine 2 functions and the parameter is the function before
 // we use this interfece to make sure that get function in "this.httpClient.get" will return the same structurfor interface
 // this interface is the same for structure dor the data returned so we shiuld check the structure before. 
-interface GetResponse {
+interface GetProductResponse {
   _embedded: {
     products: Product[];
+  }
+}
+
+interface GetProductCategoryResponse {
+  _embedded: {
+    productCategory: ProductCategory[];
   }
 }
