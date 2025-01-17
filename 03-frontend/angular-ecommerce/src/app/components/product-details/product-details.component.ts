a1da5d6911b14ed9ed66e031b52cd12b59311727
Omar Reda <omarredaelsayied@gmail.com>
2025-01-17 08:23:32 +0200

    refactor cart service, add cart status to details view

---
+++ 03-frontend/angular-ecommerce/src/app/components/product-details/product-details.component.ts
@@ -0,0 +1,48 @@
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from './../../services/product.service';
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/common/product';
+import { CartService } from 'src/app/services/cart.service';
+import { CartItem } from 'src/app/common/cart-item';
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.css']
+})
+export class ProductDetailsComponent implements OnInit {
+
+
+  product!: Product;
+
+
+  constructor(private productService: ProductService,
+    private route: ActivatedRoute,
+    private cartService: CartService) { }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(() => {
+      this.handleProductDetails()
+    })
+  }
+
+
+  handleProductDetails() {
+    const theProductId: number = +(this.route.snapshot.paramMap.get('id'))!
+
+    this.productService.getProduct(theProductId).subscribe(
+      data => {
+        this.product = data
+      }
+    )
+  }
+
+
+
+  addToCart() {
+    console.log(`Adding to cart: ${this.product.name}, ${this.product.unitPrice}`);
+    const theCartItem = new CartItem(this.product);
+    this.cartService.addToCart(theCartItem);
+  }
+
+}
---
+++ 03-frontend/angular-ecommerce/src/app/services/cart.service.ts
@@ -0,0 +1,83 @@
+import { Injectable } from '@angular/core';
+import { CartItem } from '../common/cart-item';
+import { Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+
+  cartItem: CartItem[] = [];
+
+  totalPrice: Subject<number> = new Subject<number>();
+  totalQuantity: Subject<number> = new Subject<number>();
+
+  constructor() { }
+
+
+  addToCart(theCartItem: CartItem) {
+
+    //check if we already have the item in our cart
+    let alreadyExistInCart: boolean = false;
+    let existingCartItem: CartItem | undefined = undefined;
+
+    if (this.cartItem.length > 0) {
+      // find the item in the cart based on item id
+      existingCartItem = this.cartItem.find(tempCartItem => tempCartItem.id === theCartItem.id);
+      
+    }
+
+    //check if we found it
+    alreadyExistInCart = (existingCartItem != undefined)
+    if (alreadyExistInCart) {
+      // increment the quantity
+      existingCartItem!.quantity++;
+    } else {
+      // just add the item to the array
+      this.cartItem.push(theCartItem);
+    }
+
+
+    // compute cart total price and quantity
+    this.computeCartTotals()
+  }
+
+
+
+  computeCartTotals() {
+    let totalPriceValue:number = 0;
+    let totalQuantityValue:number = 0;
+
+    for(let currentCartItem of this.cartItem){
+      totalPriceValue += currentCartItem.unitPrice * currentCartItem.quantity;
+      totalQuantityValue += currentCartItem.quantity;
+    }
+
+
+    // publish the new values ... all subscribers wiil recieve the new data
+    this.totalPrice.next(totalPriceValue);
+    this.totalQuantity.next(totalQuantityValue);
+
+
+    // log cart data just for debugging purpose
+    this.logCartData(totalPriceValue, totalQuantityValue)
+
+  }
+
+  logCartData(totalPriceValue: number, totalQuantityValue: number) {
+    
+    console.log("Content of the cart ");
+    for (const tempCartItem of this.cartItem) {
+      const subtotalPrice = tempCartItem.quantity * tempCartItem.unitPrice;
+      console.log(`name: ${tempCartItem.name}, quantity: ${tempCartItem.quantity},
+                   unitPrice: ${tempCartItem.unitPrice}, subTotalPricer: ${subtotalPrice}`);
+        
+    }
+
+    console.log(`totalPriceValue: ${totalPriceValue.toFixed(2)}, totalQuantityValue: ${totalQuantityValue}`);
+    
+    
+  }
+}
+
+
